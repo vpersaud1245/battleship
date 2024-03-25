53f8e9c6d088e5bea59afcb6e10e59ddbd02c27b
Vishwanauth Persaud <vpersaud1245@gmail.com>
2024-03-25 16:56:40 -0400

    test: Add tests for gameboard placeShip method

---
+++ tests/gameboard.test.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-undef */
+import Gameboard from "../src/gameboard";
+
+describe("Gameboard", () => {
+  let gameboard;
+
+  beforeEach(() => {
+    gameboard = new Gameboard();
+  });
+
+  it("Converts coordinate to array index", () => {
+    expect(gameboard.getIndex(5)).toBe(4);
+    expect(gameboard.getIndex(2)).toBe(1);
+  });
+
+  it("Places ship at given coordinates along the X axis", () => {
+    gameboard.placeShip(1, 1, 5, "X");
+    expect(gameboard.gameboard[0][0]).not.toBeUndefined();
+    expect(gameboard.gameboard[0][1]).not.toBeUndefined();
+    expect(gameboard.gameboard[0][2]).not.toBeUndefined();
+    expect(gameboard.gameboard[0][3]).not.toBeUndefined();
+    expect(gameboard.gameboard[0][4]).not.toBeUndefined();
+    expect(gameboard.gameboard[0][5]).toBeUndefined();
+    gameboard.placeShip(2, 2, 3, "X");
+    expect(gameboard.gameboard[1][1]).not.toBeUndefined();
+    expect(gameboard.gameboard[1][2]).not.toBeUndefined();
+    expect(gameboard.gameboard[1][3]).not.toBeUndefined();
+  });
+
+  it("Places ship at given coordinates along the Y axis", () => {
+    gameboard.placeShip(1, 1, 3, "Y");
+    expect(gameboard.gameboard[0][0]).not.toBeUndefined();
+    expect(gameboard.gameboard[1][0]).not.toBeUndefined();
+    expect(gameboard.gameboard[2][0]).not.toBeUndefined();
+    expect(gameboard.gameboard[2][3]).toBeUndefined();
+  });
+});
